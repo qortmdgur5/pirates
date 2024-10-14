cd8b43d2931728446899806c6aa6e12ff5fe0eea
bsh <dnfg12@naver.com>
2024-10-14 19:14:20 +0900

    관리자 게스트하우스 승인 모달 컴포넌트 코드위치 수정 및 디자인 적용

--- frontend/src/components/common/button/ApproveButton.tsx
+++ frontend/src/components/common/button/ApproveButton.tsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
-import Modal from "react-modal";
 import styled from "styled-components";
 
 interface ApproveInterface {
   isApprove: boolean;
+  onClick: () => void;
 }
 
 const ApproveButtonBox = styled.button`
@@ -19,74 +18,17 @@ const ApproveButtonBox = styled.button`
   }
 `;
 
-const customModalStyles: ReactModal.Styles = {
-  overlay: {
-    backgroundColor: " rgba(0, 0, 0, 0.4)",
-    width: "100%",
-    height: "100vh",
-    zIndex: "10",
-    position: "fixed",
-    top: "0",
-    left: "0",
-  },
-  content: {
-    width: "360px",
-    height: "180px",
-    zIndex: "150",
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    borderRadius: "10px",
-    boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.25)",
-    backgroundColor: "white",
-    justifyContent: "center",
-    overflow: "auto",
-  },
-};
-
-Modal.setAppElement("#root"); // 앱의 최상위 요소를 설정
-
-function ApproveButton({ isApprove }: ApproveInterface) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
-
-  const handleApprove = () => {
-    // 승인 처리 로직 (예: 서버로 데이터 전송 등)
-    console.log("승인 처리 완료");
-    closeModal(); // 모달 닫기
-  };
-
+function ApproveButton({ isApprove, onClick }: ApproveInterface) {
   return (
     <>
-      <ApproveButtonBox onClick={openModal} disabled={isApprove}>
-        {isApprove ? (
-          <img
-            src="/src/assets/image/approvedButton.png"
-            alt="approvedButton_img"
-          />
-        ) : (
-          <img
-            src="/src/assets/image/approveButton.png"
-            alt="approveButton_img"
-          />
-        )}
+      <ApproveButtonBox onClick={onClick} disabled={isApprove}>
+        <img
+          src={`/src/assets/image/${
+            isApprove ? "approvedButton.png" : "approveButton.png"
+          }`}
+          alt="approve_button"
+        />
       </ApproveButtonBox>
-
-      {/* 모달 컴포넌트 */}
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-        contentLabel="승인 확인 모달"
-        style={customModalStyles}
-        ariaHideApp={false}
-      >
-        <h2>승인하시겠습니까?</h2>
-        <button onClick={handleApprove}>승인</button>
-        <button onClick={closeModal}>취소</button>
-      </Modal>
     </>
   );
 }
--- frontend/src/pages/admin/houseApprove/components/HouseApproveTable.tsx
+++ frontend/src/pages/admin/houseApprove/components/HouseApproveTable.tsx
@@ -1,8 +1,12 @@
-import styles from "./styles/houseApproveTable.module.scss"
+import styles from "./styles/houseApproveTable.module.scss";
+import Modal from "react-modal";
 
 // 컴포넌트
 import ApproveButton from "../../../../components/common/button/ApproveButton";
 import DenyButton from "../../../../components/common/button/DenyButton";
+import { useState } from "react";
+
+Modal.setAppElement("#root"); // 앱의 최상위 요소를 설정
 
 function HouseApproveTable() {
   const data = [
@@ -29,6 +33,46 @@ function HouseApproveTable() {
     },
   ];
 
+  const customModalStyles: ReactModal.Styles = {
+    // overlay 모달 창 외부 영역 디자인
+    overlay: {
+      backgroundColor: " rgba(0, 0, 0, 0.529)",
+      width: "100%",
+      height: "100vh",
+      zIndex: "10",
+      position: "fixed",
+      top: "0",
+      left: "0",
+    },
+    // content 모달 창 영역 디자인
+    content: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+      width: "845px",
+      height: "fit-content",
+      zIndex: "20",
+      position: "absolute",
+      top: "85%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      borderRadius: "30px",
+      boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.25)",
+      backgroundColor: "white",
+      padding: "45px 0 20px 0",
+    },
+  };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  const handleApprove = () => {
+    closeModal(); // 모달 닫기
+  };
+
   return (
     <div className={styles.table_container}>
       <table className={styles.guest_house_table}>
@@ -53,16 +97,35 @@ function HouseApproveTable() {
               <td className={styles.text_left}>{item.name}</td>
               <td className={styles.text_left}>{item.id}</td>
               <td className={styles.text_left}>{item.phone}</td>
-              <td className={styles.text_center}>{item.isApprove ? "Yes" : "No"}</td>
-              <td className={styles.text_center}><ApproveButton isApprove={item.isApprove} /></td>
-              <td className={styles.text_center}><DenyButton /></td>
+              <td className={styles.text_center}>
+                {item.isApprove ? "Yes" : "No"}
+              </td>
+              <td className={styles.text_center}>
+                <ApproveButton isApprove={item.isApprove} onClick={openModal}/>
+              </td>
+              <td className={styles.text_center}>
+                <DenyButton />
+              </td>
               <td className={styles.td_right_black}></td>
             </tr>
           ))}
         </tbody>
       </table>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="승인 확인 모달"
+        style={customModalStyles}
+        ariaHideApp={false}
+      >
+        <p className={styles.modal_text_1}>"김철수" 님을 승인하시겠습니까?</p>
+        <div className={styles.modal_button_box}>
+          <button className={styles.modal_blue_button}>확인</button>
+          <button className={styles.modal_gray_button} onClick={closeModal}>취소</button>
+        </div>
+      </Modal>
     </div>
   );
 }
 
-export default HouseApproveTable
+export default HouseApproveTable;
