70a56f636e8cc5ed174d48d3363fbba9372058c4
bsh <dnfg12@naver.com>
2024-10-16 16:59:51 +0900

    매니저 승인관리 페이지 승인 및 취소 모달 작업 완료

--- frontend/src/pages/manager/managerApprove/components/ManagerApproveTable.tsx
+++ frontend/src/pages/manager/managerApprove/components/ManagerApproveTable.tsx
@@ -1,8 +1,13 @@
 import styles from "./styles/managerApproveTable.module.scss"
+import Modal from "react-modal";
 
 // 컴포넌트
 import ApproveButton from "../../../../components/common/button/ApproveButton";
 import DenyButton from "../../../../components/common/button/DenyButton";
+import { useState } from "react";
+
+Modal.setAppElement("#root"); // 앱의 최상위 요소를 설정
+
 
 function ManagerApproveTable() {
   const data = [
@@ -32,6 +37,49 @@ function ManagerApproveTable() {
     },
   ];
 
+  const customModalStyles: ReactModal.Styles = {
+    // overlay 모달 창 외부 영역 디자인
+    overlay: {
+      backgroundColor: " rgba(0, 0, 0, 0.529)",
+      width: "100%",
+      height: "100vh",
+      zIndex: "10",
+      position: "fixed",
+      top: "0",
+      left: "0",
+    },
+    // content 모달 창 영역 디자인
+    content: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+      width: "845px",
+      height: "fit-content",
+      zIndex: "20",
+      position: "absolute",
+      top: "85%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      borderRadius: "30px",
+      boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.25)",
+      backgroundColor: "white",
+      padding: "45px 0 20px 0",
+    },
+  };
+
+  // 승인 확인 모달 상태 관리
+  const [isApproveModalOpen, setIsApproveModalOpen] = useState(false);
+
+  const openApproveModal = () => setIsApproveModalOpen(true);
+  const closeApproveModal = () => setIsApproveModalOpen(false);
+
+  // 승인 취소 모달 상태 관리
+  const [isDenyModalOpen, setIsDenyModalOpen] = useState(false);
+
+  const openDenyModal = () => setIsDenyModalOpen(true);
+  const closeDenyModal = () => setIsDenyModalOpen(false);
+
   return (
     <div className={styles.table_container}>
       <table className={styles.guest_house_table}>
@@ -59,13 +107,39 @@ function ManagerApproveTable() {
               <td className={styles.text_left}>{item.phone}</td>
               <td className={styles.text_left}>{item.date}</td>
               <td className={styles.text_center}>{item.isApprove ? "Yes" : "No"}</td>
-              <td className={styles.text_center}><ApproveButton isApprove={item.isApprove} /></td>
-              <td className={styles.text_center}><DenyButton /></td>
+              <td className={styles.text_center}><ApproveButton isApprove={item.isApprove} onClick={openApproveModal} /></td>
+              <td className={styles.text_center}><DenyButton onClick={openDenyModal} /></td>
               <td className={styles.td_right_black}></td>
             </tr>
           ))}
         </tbody>
       </table>
+      <Modal
+        isOpen={isApproveModalOpen}
+        onRequestClose={closeApproveModal}
+        contentLabel="승인 확인 모달"
+        style={customModalStyles}
+        ariaHideApp={false}
+      >
+        <p className={styles.modal_text_1}>"김철수" 님을 승인하시겠습니까?</p>
+        <div className={styles.modal_button_box}>
+          <button className={styles.modal_blue_button}>확인</button>
+          <button className={styles.modal_gray_button} onClick={closeApproveModal}>취소</button>
+        </div>
+      </Modal>
+      <Modal
+        isOpen={isDenyModalOpen}
+        onRequestClose={closeDenyModal}
+        contentLabel="승인 취소 모달"
+        style={customModalStyles}
+        ariaHideApp={false}
+      >
+        <p className={styles.modal_text_1}>"김철수" 님을 취소하시겠습니까?</p>
+        <div className={styles.modal_button_box}>
+          <button className={styles.modal_blue_button}>확인</button>
+          <button className={styles.modal_gray_button} onClick={closeDenyModal}>취소</button>
+        </div>
+      </Modal>
     </div>
   );
 }
