a4ac2082b37d79371e61d6b4f9df7d55eda32cb4
bsh <dnfg12@naver.com>
2024-11-08 13:48:34 +0900

    Revert "API 다른 서버에서 받아와서 테스트, 우리 서버가 문제인지 확인위함"
    This reverts commit d6c819bc1a544e8a02eca7bb5a2a6c2386c9cb84.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,6 @@ import ManagerApprovePage from "./pages/manager/managerApprove/ManagerApprove";
 import ManagePartyPage from "./pages/manager/manageParty/ManageParty"
 import ManagePartyDetailPage from "./pages/manager/managePartyDetail/ManagePartyDetail"
 import OwnerSignupPage from "./pages/owner/signup/Signup"
-import TestPage from "./pages/test/Test"
 
 function App() {
   return (
@@ -23,7 +22,6 @@ function App() {
         <Route path="/manager/manageParty" element={<ManagePartyPage />}></Route>
         <Route path="/manager/managePartyDetail/:id" element={<ManagePartyDetailPage />}></Route>
         <Route path="/owner/signup" element={<OwnerSignupPage />}></Route>
-        <Route path="/test" element={<TestPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
