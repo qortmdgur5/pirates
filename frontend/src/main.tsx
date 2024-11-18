82c9a6746ac48343c0b9d8c6c1d4bc37b5210e27
bsh <dnfg12@naver.com>
2024-11-18 14:17:23 +0900

    recoil 상태관리 라이브러리 사용하기 위해서는 App 컴포넌트 RecoilRoot 로 감싸줘야함. 버그 수정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,9 @@
 import { createRoot } from "react-dom/client";
+import { RecoilRoot } from "recoil";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>
+);
