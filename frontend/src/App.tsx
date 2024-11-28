8d551cd374ad58084e195105b85f0a1f6f98dded
bsh <dnfg12@naver.com>
2024-11-28 17:57:34 +0900

    기본 인덱스 페이지 유저 로그인 페이지로 변경 및 매니저 사장님 로그인 페이지는 /manager/login 으로 변경 And 유저 로그인 페이지 제작 완료

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,19 +9,21 @@ import ManagerApprovePage from "./pages/owner/managerApprove/ManagerApprove";
 import ManagePartyPage from "./pages/manager/manageParty/ManageParty"
 import ManagePartyDetailPage from "./pages/manager/managePartyDetail/ManagePartyDetail"
 import ManagerSignupPage from "./pages/manager/signup/Signup"
+import UserLoginPage from "./pages/user/login/Login"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index path="/" element={<ManagerLoginPage />}></Route>
         <Route path="/admin/houseManage" element={<AdminHouseManagePage />}></Route>
         <Route path="/admin/houseApprove" element={<AdminHouseApprovePage />}></Route>
         <Route path="/owner/manageHouse" element={<ManageHousePage />}></Route>
         <Route path="/owner/managerApprove" element={<ManagerApprovePage />}></Route>
+        <Route path="/manager/login" element={<ManagerLoginPage />}></Route>
         <Route path="/manager/manageParty" element={<ManagePartyPage />}></Route>
         <Route path="/manager/managePartyDetail/:id" element={<ManagePartyDetailPage />}></Route>
         <Route path="/manager/signup" element={<ManagerSignupPage />}></Route>
+        <Route index path="/" element={<UserLoginPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/user/login/Login.tsx
+++ frontend/src/pages/user/login/Login.tsx
@@ -5,7 +5,39 @@ function Login() {
   // 네비게이션 함수
   const navigation = useNavigation();
 
-  return null;
+  return (
+    <div className={styles.container}>
+      <div className={styles.container_contents}>
+        <div className={styles.logo_text_box}>
+          <p className={styles.logo_text_1}>해적</p>
+          <p className={styles.logo_text_2}>Pirates</p>
+          <p className={styles.logo_text_3}>게스트 하우스 정보가 한눈에!</p>
+        </div>
+        <div className={styles.logo_box}>
+          <img src="/src/assets/image/pirates_logo_img.png" alt="main_logo" />
+        </div>
+        <p className={styles.login_text_1}>
+          회원 서비스 이용을 위해 로그인 해주세요.
+        </p>
+        <div className={styles.sns_login_box}>
+          <button className={styles.google_login_button}>
+            <img
+              src="/src/assets/image/google_login_button.png"
+              alt="google_login_img"
+            />
+            <span>구글 로그인</span>
+          </button>
+          <button className={styles.kakao_login_button}>
+            <img
+              src="/src/assets/image/kakao_login_button.png"
+              alt="kakao_login_img"
+            />
+            <span>카카오 로그인</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
