2d2dd6e8e9f88b4cec5b115044e584c0db0ec491
bsh <dnfg12@naver.com>
2025-02-08 01:44:46 +0900

    유저의 파티방 페이지 matchTime 필드명 새로 받아서 그에따른 동작방식 처리 수정

--- frontend/src/pages/user/party/Party.tsx
+++ frontend/src/pages/user/party/Party.tsx
@@ -16,25 +16,72 @@ interface PartyInfo {
   score: number | null; // 게스트 하우스 평점
   loveCount: number | null; // 게스트 하우스 짝매칭 카운트
   party_on: boolean; // 파티 오픈 상태
+  matchStartTime: string | null; // 짝매칭 시작시간
 }
 
 function Party() {
   const [partyInfo, setPartyInfo] = useState<PartyInfo | null>(null); // 파티 정보 상태 추가
+  const [matchTime, setMatchTime] = useState<string | null>(null); // 짝매칭 시작 시간
   const [loading, setLoading] = useState<boolean>(true); // 로딩 상태 추가
   const [error, setError] = useState<string | null>(null); // 에러 상태 추가
   const user = useSessionUser();
   const navigation = useNavigate();
 
+  // 파티방 전체 정보 가졍오기 API
   const getPartyInfo = async (partyId: number) => {
     try {
       const response = await axios.get(`/api/user/party/${partyId}`);
-      setPartyInfo(response.data.data[0]);
+      const data = response.data.data[0];
+      setPartyInfo(data);
+      setMatchTime(data.matchStartTime);
     } catch (err) {
       console.error("파티 정보 가져오기 오류:", err);
       setError("파티 정보를 불러오는 데 실패했습니다.");
     } finally {
       setLoading(false); // 로딩 상태 종료
     }
+  };
+
+  // 새로운 파티방 짝매칭 시작시간 가져오기 API
+  const getMatchTime = async () => {
+    if (!user?.party_id) return;
+
+    try {
+      const response = await axios.get(
+        `/api/user/party/matchTime/${user.party_id}`
+      );
+      const newMatchTime = response.data.data.matchStartTime; // 새로 받아온 시작시간
+
+      if (!newMatchTime) {
+        alert("짝매칭이 아직 시작되지 않았습니다.");
+        return;
+      }
+
+      setMatchTime(newMatchTime);
+      navigation("/user/party/loveSelect", {
+        state: {
+          houseName: partyInfo?.name,
+          matchStartTime: newMatchTime,
+        },
+      });
+    } catch (err) {
+      console.log("짝매칭 시작 시간을 가져오기 오류:", err);
+    }
+  };
+
+  const handleMatchButton = () => {
+    if (!matchTime) {
+      getMatchTime(); // matchTime이 null이면 API 호출
+      return;
+    }
+
+    // 짝매칭 시작되어서 matchTime 데이터 존재하면
+    navigation("/user/party/loveSelect", {
+      state: {
+        houseName: partyInfo?.name,
+        matchStartTime: matchTime,
+      },
+    });
   };
 
   useEffect(() => {
@@ -46,13 +93,8 @@ function Party() {
     }
   }, [user]);
 
-  if (loading) {
-    return <div>로딩 중...</div>; // 로딩 중 상태 표시
-  }
-
-  if (error) {
-    return <div>{error}</div>; // 오류 표시
-  }
+  if (loading) return <div>로딩 중...</div>;
+  if (error) return <div>{error}</div>;
 
   return (
     <div className={styles.container}>
@@ -120,7 +162,11 @@ function Party() {
               >
                 개인 채팅방
               </button>
-              <button className={styles.love_button} type="button">
+              <button
+                className={styles.love_button}
+                type="button"
+                onClick={() => handleMatchButton()}
+              >
                 짝 매칭
               </button>
             </div>
